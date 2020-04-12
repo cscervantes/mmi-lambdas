2b24bc386f3a1c24cad900229f2607ce0d9794fb
chris <christian.cervantes@media-meter.com>
2020-04-12 18:30:24 +0800

    updated websiteRoute.js

--- lambda-server/routes/websiteRoute.js
+++ lambda-server/routes/websiteRoute.js
@@ -17,8 +17,10 @@ module.exports = function(name, router){
         try {
             
             const _req_url = req.body.url
+
+            const _request_source = req.body.request_source
             
-            const _uri = new url_helper(_req_url)
+            const _uri = new url_helper(_req_url, _request_source)
 
             const _uri_response = await _uri.MAKE_REQUEST()
 
@@ -38,10 +40,12 @@ module.exports = function(name, router){
     router.post(name+'/new_website', async function(req, res, next){
         
         try {
-            
+            console.log(req.body)
             const _req_url = req.body.url
+
+            const request_source = req.body.request_source
             
-            const _uri = new url_helper(_req_url)
+            const _uri = new url_helper(_req_url, request_source)
 
             const _uri_response = await _uri.MAKE_REQUEST()
 
@@ -57,6 +61,12 @@ module.exports = function(name, router){
 
             let $ = await _htm.HTML()
 
+            let icos = $('link').filter((i,e)=>{
+               return $(e).attr('href').search(/\.ico$/gi) != -1 || $(e).attr('href').search(/icon/gi) != -1
+            }).get()
+            
+            const fav_icon = icos.map(v=>$(v).attr('href')).slice(-1)[0]
+
             const title = ($('title').length > 0) ? $('title').text() : $('meta[property="og:title"]').attr('content')
 
             const lang = ($('html').attr('lang')) ? $('html').attr('lang') : 'en'
@@ -68,7 +78,7 @@ module.exports = function(name, router){
             const country_code = await country_language_helper.country_lists(alexa_rankings.country)
 
             const data = {
-                url, home_url, fqdn, title, language, country_code, alexa_rankings
+                url, home_url, fqdn, title, language, country_code, alexa_rankings, fav_icon, request_source
             }
             // https://www.similarweb.com/website/abs-cbn.com create function for ranking
             res.status(200).send(data)
