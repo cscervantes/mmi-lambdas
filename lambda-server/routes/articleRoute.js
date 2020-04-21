f9bfbedc6372cc6f7c80d3e59ebaffa1f61c3473
chris <christian.cervantes@media-meter.com>
2020-04-21 08:57:09 +0800

    updated article route

--- lambda-server/routes/articleRoute.js
+++ lambda-server/routes/articleRoute.js
@@ -8,7 +8,7 @@ module.exports = function(name, router){
     router.get(name, function(req, res, next){
 
         res.status(200).send('Article lambda endpoint.')
-        
+
     })
 
     router.post(name+'/test_article', async function(req, res, next){
@@ -190,6 +190,162 @@ module.exports = function(name, router){
                     })
                 }else{
 
+                    if(req.body.hasOwnProperty('website')){
+
+                        const request_source = get_website.request_source
+
+                        const home_url = get_website.website_url
+                        
+                        const startHttps = get_website.needs_https
+
+                        const endSlash = get_website.needs_endslash
+
+                        const selectors = get_website.selectors
+
+                        const code = get_website.code_snippet
+
+                        const is_using_selectors = JSON.parse(get_website.is_using_selectors)
+
+                        const is_using_snippets = JSON.parse(get_website.is_using_snippets)
+
+                        const website_cost = get_website.website_cost
+
+                        const global_rank = get_website.alexa_rankings.global
+
+                        const local_rank = get_website.alexa_rankings.local
+
+                        const _uri = new url_helper(clean_url, request_source, startHttps, endSlash)
+
+                        const _uri_response = await _uri.MAKE_REQUEST()
+
+                        const url = await _uri.FORMATTED_URL()
+
+                        const _htm = new html_helper(_uri_response, home_url, startHttps, endSlash)
+
+                        const _raw_html = await _htm.HTML()
+
+                        if(is_using_selectors){
+
+                            const _article = new article_helper(_raw_html, selectors)
+
+                            const title = await _article.ARTICLE_TITLE()
+
+                            const date = await _article.ARTICLE_PUBLISH()
+
+                            const author = await _article.ARTICLE_AUTHOR()
+
+                            const section = await _article.ARTICLE_SECTION()
+
+                            // const html = await _article.ARTICLE_HTML()
+
+                            const text = await _article.ARTICLE_TEXT()
+
+                            const image = await _article.ARTICLE_IMAGE()
+
+                            const video = await _article.ARTICLE_VIDEO()
+
+                            const values = await media_value_helper(global_rank, local_rank, website_cost, text, image, video)
+
+                            const advalue = values.advalue
+
+                            const prvalue = values.prvalue
+
+                            // const data = {url, title, date, author, section, html, text, image, video, advalue, prvalue}
+
+                            // res.status(200).send(data)
+
+                            req.body.article_url = url
+
+                            req.body.article_title = title
+
+                            req.body.article_author = author
+
+                            req.body.article_publish_date = date
+
+                            req.body.article_section = section
+
+                            req.body.article_content = text
+
+                            req.body.article_images = image
+
+                            req.body.article_videos = video
+
+                            req.body.article_ad_value = advalue
+
+                            req.body.article_pr_value = prvalue
+
+                            req.body.article_status = 'Done'
+
+                        }else if(is_using_snippets){
+
+                            const Snippet = module.exports = Function(code)()
+
+                            const _article = new Snippet(_raw_html, $string, $moment)
+
+                            const title = await _article.ARTICLE_TITLE()
+
+                            const date = await _article.ARTICLE_PUBLISH()
+
+                            const author = await _article.ARTICLE_AUTHOR()
+
+                            const section = await _article.ARTICLE_SECTION()
+
+                            // const html = await _article.ARTICLE_HTML()
+
+                            const text = await _article.ARTICLE_TEXT()
+
+                            const image = await _article.ARTICLE_IMAGE()
+
+                            const video = await _article.ARTICLE_VIDEO()
+
+                            const values = await media_value_helper(global_rank, local_rank, website_cost, text, image, video)
+                            
+                            const advalue = values.advalue
+
+                            const prvalue = values.prvalue
+
+                            // const data = {url, title, date, author, section, html, text, image, video, advalue, prvalue}
+
+                            // res.status(200).send(data)
+
+
+                            req.body.article_url = url
+
+                            req.body.article_title = title
+
+                            req.body.article_author = author
+
+                            req.body.article_publish_date = date
+
+                            req.body.article_section = section
+
+                            req.body.article_content = text
+
+                            req.body.article_images = image
+
+                            req.body.article_videos = video
+
+                            req.body.article_ad_value = advalue
+
+                            req.body.article_pr_value = prvalue
+
+                            req.body.article_status = 'Done'
+
+                        }else{
+
+                            // next('Selectors and Snippets are not configured!')
+                            res.status(500).send({
+
+                                error: 'Selectors and Snippets are not configured!',
+            
+                                error_status: 'Parser Issue!'
+            
+                            })
+
+                        }
+
+                    }
+
                     let data = await transaction_helper.STORE_ARTICLE(req)
 
                     res.status(200).send(data)
@@ -207,8 +363,11 @@ module.exports = function(name, router){
                 })
             }
         } catch (error) {
-
-            next(error)
+            req.body.article_url = encodeURI(req.body.article_url)
+            req.body.article_status = 'Error'
+            req.body.article_error_status = error
+            let storeError = await transaction_helper.STORE_ARTICLE(req)
+            next(storeError)
 
         }
 
