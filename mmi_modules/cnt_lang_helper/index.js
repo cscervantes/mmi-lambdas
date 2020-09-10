d4dd5123e8434c99af7e88901790b6aeb2c293e7
christian <christian.cervantes@media-meter.com>
2020-09-10 19:49:07 +0800

    updated cnt_lang_helper/index.js

--- mmi_modules/cnt_lang_helper/index.js
+++ mmi_modules/cnt_lang_helper/index.js
@@ -4,7 +4,7 @@ module.exports.country_lists = async function(needle){
     const promise = new Promise((resolve, reject) => {
         try {
             let countries = JSON.parse(fs.readFileSync(dir+'/country_lists.json', 'utf-8'))
-            let iso = countries.find(({country}) => country.search(new RegExp(needle, 'gi')) != -1)
+            let iso = countries.find(({country}) => country.search(new RegExp(`^${needle}`, 'gi')) != -1)
             resolve(iso.iso_codes)
         } catch (error) {
             // console.log(error)
@@ -18,7 +18,7 @@ module.exports.language_lists = async function(needle){
     const promise = new Promise((resolve, reject) => {
         try {
             let languages = JSON.parse(fs.readFileSync(dir+'/language_lists.json', 'utf-8'))
-            let lang = languages.find(({iso_2_codes}) => iso_2_codes.search(new RegExp(needle, 'gi')) != -1)
+            let lang = languages.find(({iso_2_codes}) => iso_2_codes.search(new RegExp(`^${needle}`, 'gi')) != -1)
             resolve(lang.english_name)
         } catch (error) {
             // console.log(error)
